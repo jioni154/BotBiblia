d92b42c88c4aafd34c80a02c1d254da54e9db463
jioni154 <31070478+jioni154@users.noreply.github.com>
2024-12-12 17:50:04 -0300

    superamen y ayuda agregado, refactor TODO el codigo

--- leerLaBiblia.py
+++ leerLaBiblia.py
@@ -2,11 +2,35 @@ import  json
 import re
 
 
+
+
+
 with open('es_rvr.json', 'r', encoding='UTF-8') as file:
     data =  json.load(file)
 
 
-
+def existePalabraEnCapitulosAvanzado(palabra):
+    contador=0
+    versiculosEncontrados=[]
+    contadorPalabrasEncontradas=[]
+    palabra = palabra.lower()
+    for libro in  data:
+        contadorCapitulo=0
+        for capitulos in libro['chapters']:
+            contadorVersiculo=0
+            contadorCapitulo+=1
+            for versiculo in capitulos:
+                contadorVersiculo+=1
+                versiculo = versiculo.lower()
+                encontradas = re.findall(r'\b' + palabra + r'\b', versiculo)
+                if  encontradas !=  []:
+                    contadorPalabrasEncontradas = len(encontradas)
+                    contador+=contadorPalabrasEncontradas
+                    versiculosEncontrados.append(libro['name']+f' {contadorCapitulo}:{contadorVersiculo}')
+    if contador != 0:
+        versiculosEncontrados = list(dict.fromkeys(versiculosEncontrados))
+        return True, contador, versiculosEncontrados                
+    return False, contador, versiculosEncontrados
 
 def existePalabraEnCapitulos(palabra):
     contador=0
--- main.py
+++ main.py
@@ -1,33 +1,77 @@
 import discord
 import leerLaBiblia
 import config
+from discord.ext import commands
+import PaginatorExpress
 
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+
+bot= commands.Bot(command_prefix='+',intents=discord.Intents.all()) 
+    
+@bot.event    
+async def on_ready():
+    print(f'Logged on as me!')
+ 
+@bot.command()
+async def ayuda(ctx):
+    embedDeAyuda=discord.Embed(title='Lista de comandos:')
+    embedDeAyuda.set_author(name='+ayuda')
+    embedDeAyuda.set_footer(text='Palabra de dios')
+    embedDeAyuda.add_field(name="+amen expresion", value="Busca la expresion en la biblia, te dice si existe, cantidad de veces que se repite y en que libros.",inline=False)
+    embedDeAyuda.add_field(name="+superAmen", value='Busca la expresion en la biblia, te dice si existe, la cantidad de veces que se repite y te devuelve cada versiculo con el formato "Libro capitulo:versiculo" en el que aparece la palabra',inline=False)
+    await ctx.reply(embed=embedDeAyuda)
+     
+ 
+@bot.command()
+async def amen(ctx,*args):
+    message = ' '.join(args)
+    existe, cantidad, librosDonde = leerLaBiblia.existePalabraEnCapitulos(message)
+    if existe:
+        nombreDeLibros = leerLaBiblia.conseguirNombreDeLibros(librosDonde)
+        await ctx.message.reply(f"La expresion \"{message}\" se encuentra {cantidad} de veces en la biblia.\nAparece en los libros {nombreDeLibros}")
+    else:
+        await ctx.message.reply(f'La expresion \"{message}\" no se encuentra en la biblia')
         
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
-        elif message.content.startswith('+amen'):
-            message.content = message.content.replace('+amen','')
-            message.content = message.content.strip()
-            existe, cantidad, librosDonde = leerLaBiblia.existePalabraEnCapitulos(message.content)
-            if existe:
-                nombreDeLibros = leerLaBiblia.conseguirNombreDeLibros(librosDonde)
-                await message.reply(f"La expresion \"{message.content}\" se encuentra {cantidad} de veces en la biblia.\nAparece en los libros {nombreDeLibros}")
-            else:
-                await message.reply(f'La expresion \"{message.content}\" no se encuentra en la biblia')
-        elif message.content == 'morite bot de mierda' and message.author.name == 'juanmateo':
-            await client.close()  
         
-intents = discord.Intents.default()
-intents.message_content = True
+@bot.command()
+async def moriteBotDeMierda(ctx):
+    if ctx.author.name == 'juanmateo':
+        await ctx.message.reply('Bueno 😔😔😔😔 adios....')
+        await bot.close()   
+    else:
+        await ctx.message.reply('No me decis que hacer JUICIO FINAL')
+ 
+@bot.command()
+async def superAmen(ctx,*args):
+    message = ' '.join(args)
+    existe, cantidad, versiculosDonde = leerLaBiblia.existePalabraEnCapitulosAvanzado(message)
+    if existe:
+        embeds=[]
+        contadorEmbed=0
+        maxContadorEmbed=20
+        versiculosAcumulados=''
 
-client = Client(intents=intents)
-client.run(config.token)
-
-
-            
-            
+        for versiculo in versiculosDonde:
+            if contadorEmbed==maxContadorEmbed:
+                contadorEmbed=0
+                embedCreado=discord.Embed(title=message,description=versiculosAcumulados)
+                embedCreado.set_author(name=f'La expresion \"{message}\" se encuentra {cantidad} de veces en la biblia.')
+                embedCreado.set_footer(text=f'Pedido por {ctx.author.nick}')
+                embeds.append(embedCreado)
+                versiculosAcumulados=''
+            versiculosAcumulados= versiculosAcumulados+versiculo+f'\n'
+            contadorEmbed+=1
+        if contadorEmbed!=0:
+            embedCreado=discord.Embed(title=message,description=versiculosAcumulados)
+            embedCreado.set_author(name=f'La expresion \"{message}\" se encuentra {cantidad} de veces en la biblia.')
+            embedCreado.set_footer(text=f'Pedido por {ctx.author.nick}')
+            embeds.append(embedCreado)
+        await PaginatorExpress.Simple().start(ctx, pages= embeds)
+    else:
+        await message.reply(f'La expresion \"{message}\" no se encuentra en la biblia')
+ 
+ 
+ 
+ 
+    
+bot.run(config.token)
 
